3cb0442d4e78c058c99f18d64256b4d9ca39d75d
Arif Momin <148381072+arifmomin@users.noreply.github.com>
2023-12-09 22:34:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// alert('are you sure open this page')
+
+// let age = prompt('How old are you?')
+
+// if(age> 18){
+//     let demo = prompt('enter your demo code')
+//     if(demo < 4){
+//         alert('welcome our web page')
+//     }
+//     else{
+//         alert('Your code is not correct')
+//     }
+// }
+// else{
+//     alert('sorry you can not see this page')
+// }
+
+
+// // =================== variabale part start ==========================
+// let one = document.querySelector('.one')
+
+// let sun = document.querySelector('.input')
+
+// let button_one = document.querySelector('.button_one')
+
+// let two = document.querySelector('.two')
+
+// let three = document.querySelector('.three')
+// // ------------------- variabale part end ----------------------------
+
+
+// // =================== console part start ==========================
+
+// console.log(one)
+
+// console.log(sun.value)
+// console.log(button_one)
+// console.log(three)
+// // ------------------- console part end ----------------------------
+
+
+// // =================== funcation part start ==========================
+// button_one.addEventListener('click', function(){
+//     two.innerHTML=sun.value
+// })
+// // ------------------- funcation part end ----------------------------
+
+
+// for(let i=1; i<11; i++){
+//     three.innerHTML += (5 + ' x ' +  i + ' = ' + 5*i + '<br>')
+
+// }
+
+// let legend =document.querySelector('.three')
+// let two =document.querySelector('.two')
+
+// let one =45
+// let four =50
+// console.log(one)
+// console.log(four)
+// console.log(one+four)
+
+// console.log(two.innerHTML)
+// console.log(legend.innerHTML)
+
+// let boyos =  prompt('please enter your number')
+// console.log(boyos)
+// if( boyos >= 20){
+//     alert('welcome to my world')
+//  } else (boyos < 20)
+// alert('please get out of my place')
+
+
+
+
+
+
+// let ones = prompt('how old are you')
+
+// if (ones > 20){
+//     alert ('welcome sir')
+// }else{
+//     alert ('jah bag sala')
+// }
+let boyos = prompt ( 'tomar boyos koto' )
+if ('boyos < 20'){
+    alert('welcome sir ')
+}else{
+  alert('jah bag ')
+}
